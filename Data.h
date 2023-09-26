f0da6416474a2ab14424d561b6f047d0c590883f
gaetaCristiano03 <128974985+gaetaCristiano03@users.noreply.github.com>
2023-09-26 10:45:19 +0200

    Aggiunto const ai metodi controlloData e stampaData, controlloData è private.

--- Data.h
+++ Data.h
@@ -11,22 +11,22 @@ class Data {
 private:
     int giorno, mese, anno;
 
+    const void controlloData(int giorno, int mese, int anno) {
+        if (giorno >= 1 && giorno <= 31 && mese >= 1 && mese <= 12) {
+            this->giorno = giorno;
+            this->mese = mese;
+            this->anno = anno;
+        }
+        else
+            throw invalid_argument("Valori non validi.");
+    }
+
 public:
     Data(int giorno, int mese, int anno) {
         controlloData(giorno, mese, anno);
     };
 
-    void controlloData(int giorno, int mese, int anno) {
-        if (giorno >= 1 && giorno <= 31 && mese >= 1 && mese <= 12) {
-            this->giorno = giorno;
-            this->mese = mese;
-            this->anno = anno;
-            }
-        else
-            throw invalid_argument("Valori non validi.");
-    }
-
-    string stampaData() {
+    const string stampaData() {
         string line = to_string(giorno) + "-" + to_string(mese) + "-" + to_string(anno);
         return line;
     }
